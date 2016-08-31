70c4c0e62191ab6cceec48cbbaae6f4946a6b57d
Oliver Swoboda <Oliver.Swoboda@mgm-tp.com>
2016-08-31 16:01:30 +0200

    test

--- querydata/src/main/java/de/oswoboda/querydata/Main.java
+++ querydata/src/main/java/de/oswoboda/querydata/Main.java
@@ -4,9 +4,9 @@ import java.util.Calendar;
 import java.util.Date;
 
 import org.kairosdb.client.HttpClient;
+import org.kairosdb.client.builder.AggregatorFactory;
 import org.kairosdb.client.builder.QueryBuilder;
-import org.kairosdb.client.builder.QueryMetric;
-import org.kairosdb.client.response.GetResponse;
+import org.kairosdb.client.builder.TimeUnit;
 import org.kairosdb.client.response.Queries;
 import org.kairosdb.client.response.QueryResponse;
 
@@ -15,26 +15,28 @@ public class Main {
     	long startTime = System.currentTimeMillis();
     	System.out.println("Start: "+startTime);
     	HttpClient client = new HttpClient("http://ring01.ext.mgm-tp.com:25025");
-    	GetResponse stationResponse = client.getTagValues();
+    	//GetResponse stationResponse = client.getTagValues();
     	
     	Calendar calendar = Calendar.getInstance();
-    	calendar.set(2010, 0, 1);
+    	calendar.set(1900, 0, 1);
     	Date start = calendar.getTime();
-    	calendar.set(2015, 0, 1);
+    	calendar.set(2017, 0, 1);
     	Date end = calendar.getTime();
     	
     	QueryBuilder builder = QueryBuilder.getInstance();
-    	QueryMetric metric = builder.setStart(start)
+    	builder.setStart(start)
     		.setEnd(end)
-    		.addMetric(args[1]);
-    	int i = 0;
+    		.addMetric(args[0])
+    		.addTag("station", args[1])
+    		.addAggregator(AggregatorFactory.createAverageAggregator(200, TimeUnit.YEARS));
+    	/*int i = 0;
     	for (String station : stationResponse.getResults()) {
     		if (i <= Integer.parseInt(args[0])) {
     			metric.addTag("station", station);
     		}
     		i++;
     	}
-    	System.out.println("Stations :"+i);
+    	System.out.println("Stations :"+i);*/
     	QueryResponse qResponse = client.query(builder);
     	long endTime = System.currentTimeMillis();
     	System.out.println("End: "+System.currentTimeMillis());
