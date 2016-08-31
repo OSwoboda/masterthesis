d0ee80dd97f7a202ea1a1ef0d556015516ea1bf1
Oliver Swoboda <Oliver.Swoboda@mgm-tp.com>
2016-08-31 15:42:49 +0200

    test

--- querydata/src/main/java/de/oswoboda/querydata/Main.java
+++ querydata/src/main/java/de/oswoboda/querydata/Main.java
@@ -18,7 +18,7 @@ public class Main {
     	GetResponse stationResponse = client.getTagValues();
     	
     	Calendar calendar = Calendar.getInstance();
-    	calendar.set(2014, 0, 1);
+    	calendar.set(2010, 0, 1);
     	Date start = calendar.getTime();
     	calendar.set(2015, 0, 1);
     	Date end = calendar.getTime();
