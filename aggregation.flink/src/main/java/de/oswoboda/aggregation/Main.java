8e12e15c469bf28ac2cca56e07e6c97f8fa3f389
Oliver Swoboda <Oliver.Swoboda@mgm-tp.com>
2016-09-22 10:58:13 +0200

    flatMap

--- aggregation.flink/src/main/java/de/oswoboda/aggregation/Main.java
+++ aggregation.flink/src/main/java/de/oswoboda/aggregation/Main.java
@@ -6,9 +6,12 @@ import org.apache.accumulo.core.client.security.tokens.PasswordToken;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.security.Authorizations;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.aggregation.Aggregations;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
 import org.apache.hadoop.mapreduce.Job;
 
 public class Main {
@@ -25,8 +28,21 @@ public class Main {
 		ClientConfiguration clientConfig = new ClientConfiguration();
 		AccumuloInputFormat.setZooKeeperInstance(job, clientConfig.withInstance("hdp-accumulo-instance").withZkHosts("localhost:2181"));
 		
-		DataSource<Tuple2<Key, Value>> source = env.createHadoopInput(new AccumuloInputFormat(), Key.class, Value.class, job);
-		source.print();
+		DataSet<Tuple2<Key, Value>> source = env.createHadoopInput(new AccumuloInputFormat(), Key.class, Value.class, job);
+		DataSet<Long> result = source.flatMap(new FlatMapFunction<Tuple2<Key,Value>, Long>() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void flatMap(Tuple2<Key, Value> in, Collector<Long> out) throws Exception {
+				Metric metric = Metric.parse(in.f0, in.f1);
+				if (metric.getStation().equals("GME00102292")) {
+					out.collect(metric.getValue());
+				}
+			}
+		}).aggregate(Aggregations.MIN, 0);
+		
+		result.print();
 
 		// execute program
 		env.execute("Accumulo Flink Aggregation");
