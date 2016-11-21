7ac893400054083f7684d38125bcacb93e16f9cd
Oliver Swoboda <Oliver.Swoboda@mgm-tp.com>
2016-11-21 13:49:07 +0100

    close test

---
+++ aggregation.flink/src/main/java/de/oswoboda/aggregation/AccInputFormat.java
@@ -0,0 +1,26 @@
+package de.oswoboda.aggregation;
+
+import java.io.IOException;
+
+import org.apache.accumulo.core.client.mapreduce.AccumuloInputFormat;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Value;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+public class AccInputFormat extends AccumuloInputFormat {
+	
+	private RecordReader<Key, Value> recordReader;
+	
+	@Override
+	public RecordReader<Key, Value> createRecordReader(InputSplit split, TaskAttemptContext context)
+			throws IOException, InterruptedException {
+		recordReader = super.createRecordReader(split, context);
+		return recordReader;
+	}
+	
+	public RecordReader<Key, Value> getRecordReader() {
+		return recordReader;
+	}
+}
